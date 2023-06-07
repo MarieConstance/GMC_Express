386ce13ca235e725d6b4ff7ae330abaf8d4b0f78
Marie Goa <mgoa90334@gmail.com>
2023-06-07 18:37:13 +0000

    ajouter

--- server.js
+++ server.js
@@ -1,33 +1,47 @@
 const express = require('express');
-const mongoose = require('mongoose')
-const app = express()
+const mongoose = require('mongoose');
+const Product = require('./model/productModel');
+const app = express();
+
+app.use(express.json());
 
 //routes
 
-app.get('/',(req,res)=>{
-    res.send('Hello NODE API')
-})
+app.get('/', (req, res) => {
+  res.send('Hello NODE API');
+});
 
-app.get('/blog',(req,res)=>{
-    res.send('Hello Blog, My name is DevMarie')
-})
+app.get('/products', async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
-app.listen(3000, ()=>{
-    console.log(`Node API app is running on port 3000`);
-})
+app.get('/blog', (req, res) => {
+  res.send('Hello Blog, My name is DevMarie');
+});
 
-mongoose.connect('mongodb+srv://marie:HYomOeFnLhtO9vyu@cluster0.ueg1rii.mongodb.net/')
-
-.then(()=> {
-    app.listen(3000, ()=>{
-        console.log(`Node API app is running on port 3000`);
-    })
-    console.log('connected to mongoDB');
-})
-
-.catch((error)=> {
+app.post('/products', async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
     console.log(error);
-})
+    res.status(500).json({ message: error.message });
+  }
+});
 
-// HYomOeFnLhtO9vyu
-// mongodb+srv://marie:HYomOeFnLhtO9vyu@cluster0.ueg1rii.mongodb.net/
+mongoose
+  .connect('mongodb+srv://marie:marie1234@cluster0.ueg1rii.mongodb.net/')
+  .then(() => {
+    app.listen(3000, () => {
+      console.log('Node API app is running on port 3000');
+    });
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log(error);
+  });
