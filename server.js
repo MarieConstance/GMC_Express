985b496e59105bbc18df6414c2ab2f921a4a2606
Marie Goa <mgoa90334@gmail.com>
2023-06-08 17:45:26 +0000

    bd

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ app.get('/', (req, res) => {
   res.send('Hello NODE API');
 });
 
+
+app.get('/blog', (req, res) => {
+  res.send('Hello Blog, My name is DevMarie');
+});
+
 app.get('/products', async (req, res) => {
   try {
     const products = await Product.find({});
@@ -18,10 +23,19 @@ app.get('/products', async (req, res) => {
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
+
 });
 
-app.get('/blog', (req, res) => {
-  res.send('Hello Blog, My name is DevMarie');
+
+app.get('/products/:id', async (req, res) => {
+  try {
+    const {id} = req/params;
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+
 });
 
 app.post('/products', async (req, res) => {
@@ -34,8 +48,29 @@ app.post('/products', async (req, res) => {
   }
 });
 
-mongoose
-  .connect('mongodb+srv://marie:marie1234@cluster0.ueg1rii.mongodb.net/')
+// update a product
+
+app.put('/products/:id', async(req, res)=>{
+  try {
+    const {id} = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body)
+    // we cannot find any product in database
+
+    if(!product){
+
+      return res.status(404).json({message: `cannot find aby product with ID ${id}`})
+    }
+    res.status(200).json(product)
+
+  } catch (error) {
+    res.status(500).json({message: error.message})
+  }
+
+})
+
+mongoose.set("strictQuery", false)
+mongoose.
+connect('mongodb+srv://marie:marie1234@cluster0.ueg1rii.mongodb.net/')
   .then(() => {
     app.listen(3000, () => {
       console.log('Node API app is running on port 3000');
